5298f2b91233dfb3a7757c6ebc50a3812eff82ac
Tyler Sanders <tylerssanders@gmail.com>
2012-08-14 11:12:30 -0500

    Default URL for webdav

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['oracle-xe']['remote_url'] = "http://192.168.0.18/webdav1/OracleXEUniv.exe"
+default['oracle-xe']['remote_url'] = "http://ch33-webdav/webdav1/OracleXEUniv.exe"
 default['oracle-xe']['response_file'] = "c:\\OracleXE-install.iss"
 default['oracle-xe']['log_file'] = "c:\\OracleXE-install.log"
 
