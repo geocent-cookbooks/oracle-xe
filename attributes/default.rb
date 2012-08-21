92244c981ec3126993e47ef943af674524fcef06
David Messina <david.messina@geocent.com>
2012-08-21 08:14:58 -0500

    added CentOS support

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,7 @@ default['oracle-xe']['remote_url'] = "http://ch33-webdav/webdav1/OracleXEUniv.ex
 default['oracle-xe']['response_file'] = "c:\\OracleXE-install.iss"
 default['oracle-xe']['log_file'] = "c:\\OracleXE-install.log"
 
-
+default['oracle-xe']['http_port'] = "8080"
+default['oracle-xe']['listener_port'] = "1521"
+default['oracle-xe']['admin_password'] = "oracle"
+default['oracle-xe']['start_on_boot'] = "y"
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,17 +10,45 @@ remote_url = node['oracle-xe']['remote_url']
 response_file = node['oracle-xe']['response_file']
 log_file = node['oracle-xe']['log_file']
 
-if File.exists?("C:\\Oracle\\OracleXE")
-  Chef::Log.info("OracleXE install dir already exists.....not installing OracleXE")
-else
-    template response_file do
-	source "silent.xml.erb"
-    end
+log "platform: #{node['platform']}"
 
-    windows_package "Oracle 10g XE" do
+case node['platform']
+  when "windows"
+    if File.exists?("C:\\Oracle\\OracleXE")
+      Chef::Log.info("OracleXE install dir already exists.....not installing OracleXE")
+    else
+      template response_file do
+        source "silent.xml.erb"
+      end
+
+      windows_package "Oracle 10g XE" do
         source remote_url
         options "/s /f1#{response_file} /f2#{log_file}"
         installer_type :installshield
         action :install
+      end
     end
-end
+
+  when "centos"
+    template "/tmp/responsefile" do
+      source "responsefile"
+    end
+
+    installer = "/tmp/oracle-xe.rpm"
+
+    remote_file installer do
+      source remote_url
+    end
+
+    package "oracle-xe" do
+      action :install
+      source installer
+      options "--nogpgcheck"
+    end
+
+    execute "oracle-xe" do
+      command "/etc/init.d/oracle-xe configure < /tmp/responsefile >> /tmp/XEsilentinstall.log"
+      action :run
+      user "root"
+    end
+  end
